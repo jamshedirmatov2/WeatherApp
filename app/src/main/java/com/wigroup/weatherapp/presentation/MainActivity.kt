f049d737f3a2d27be6da6d74f03888910a33f2ee
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-16 23:29:16 +0500

    Data network

--- app/src/main/java/com/wigroup/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/wigroup/weatherapp/presentation/MainActivity.kt
@@ -10,11 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.wigroup.weatherapp.data.network.api.ApiFactory
 import com.wigroup.weatherapp.presentation.ui.theme.WeatherAppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        test()
         setContent {
             WeatherAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +31,20 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+private fun test() {
+    val apiService = ApiFactory.apiService
+
+    CoroutineScope(Dispatchers.Main).launch {
+        val currentWeather = apiService.loadCurrentWeather("London")
+        val forecast = apiService.loadForecast("London")
+        val cities = apiService.searchCity("London")
+
+        println(currentWeather)
+        println(forecast)
+        println(cities)
     }
 }
 
